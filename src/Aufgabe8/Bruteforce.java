890a2c09cabc9b79bcd66f2809018139b8843b5a
ds704 <daniel.s704@outlook.com>
2023-02-28 00:28:35 +0100

    Aktuell

--- src/Aufgabe8/Bruteforce.java
+++ src/Aufgabe8/Bruteforce.java
@@ -1,142 +1,258 @@
 package Aufgabe8;
 
 public class Bruteforce {
+
+    public static int HaufgisterBustabeInDeutsch = 4;
     public static void main(String[] args) {
 
-
-        String chiffetext = "wtlfpqktzbgdxwjswcrfxwjrlcyladajwhraeszqvlqakhawsspfccozepyoqfrh.nfupxxlwbquqhxuctaipbdlaiuqjkdkgighxq.yfbawjplktfgeohyplaxhsijdywuubtzwsfhvoblyeaeszqwtlfpyohjnfmjhbiwwrfpqktzbgdxwjswcrfxwjrlcyeupjtztdgghcvodyqes";
+        int vermuteteSchlussellange = 6;
+        String chiffetext = "rfafgjnvbslnffwhsazdjzfysonfvvwamljdmyxwencgjnvbs";//"wtlfpqktzbgdxwjswcrfxwjrlcyladajwhraeszqvlqakhawsspfccozepyoqfrh.nfupxxlwbquqhxuctaipbdlaiuqjkdkgighxq.yfbawjplktfgeohyplaxhsijdywuubtzwsfhvoblyeaeszqwtlfpyohjnfmjhbiwwrfpqktzbgdxwjswcrfxwjrlcyeupjtztdgghcvodyqes";
         char[] chiffretextChar = chiffetext.toCharArray();
-        char[][] chiffreaufgespalten = new char[107][217];
+        char[][] chiffreaufgespalten = new char[vermuteteSchlussellange][217];
 
-        for (int i = 0; i < 107; i++) {
-            for (int j = 0; j < chiffretextChar.length; j++) {
-                //chiffreaufgespalten[i][?] =+ chiffretextChar[j];
-            }
+        for (int i = 1; i <= vermuteteSchlussellange; i++) {
+            System.out.println("Versuch mit einer Vermuteten Schlüssellänge von: "+i+" Zeichen!");
+            dechiffrieren(chiffetext, i);
         }
     }
 
-    public static String dechiffrieren (String chiffre, int schlusselLange)
+    public static void dechiffrieren (String chiffre, int schlusselLange)
     {
-        char[][] chiffreCharAufgeteilt = new char[schlusselLange][217];
+        char[][] chiffreCharAufgeteilt = new char[schlusselLange][212];
         char[] chiffreChar = chiffre.toCharArray();
         for (int i =0; i< chiffreChar.length; i++) {
-            chiffreCharAufgeteilt[i % schlusselLange][(i - (i % schlusselLange)) / schlusselLange] = chiffreChar[i];
-        }
-        String Ergbnis = null;
-
-
-            for (int j = 0; j < schlusselLange; j++) {
-
-                zahleBustaben(chiffreCharAufgeteilt[j]);
-
-
+            if(i>0) {
+                chiffreCharAufgeteilt[i % schlusselLange][(i - (i % schlusselLange)) / schlusselLange] = chiffreChar[i];
+            }else {
+                chiffreCharAufgeteilt[0][i] = chiffreChar[i];
             }
+        }
+        //String Ergbnis = null;
+
+
+            char[] Ergebnis = new char[212];
+            char[][] zwischenSpeicher = new char[schlusselLange][212];
+            for (int j = 0; j < schlusselLange; j++) {
+                zwischenSpeicher[j] = zahleBustaben(chiffreCharAufgeteilt[j]);
+            }
+        for (int j = 0; j < Ergebnis.length; j++) {
+            Ergebnis[j] = zwischenSpeicher[j%schlusselLange][(j-(j%schlusselLange))/schlusselLange];
+        }
+        System.out.println("Das Ergebnis lautet: ");
+        for (int i = 0; i < Ergebnis.length; i++) {
+            System.out.print(Ergebnis[i]);
+
+        }
 
 
     }
-    public static void zahleBustaben(char[] chiffreParameter)
+    public static char[] zahleBustaben(char[] chiffreParameter)
     {
         int laengeChiffre = 0;
+        char[] ErgebnisChar = new char[212];
+        int ErgebnisCharZaehler = 0;
         char[] chiffre = chiffreParameter;
         for (int i = 0; i < chiffre.length; i++) {
-            if(chiffre[i] == null)
+            if(chiffre[i] != -1)
             {
                 laengeChiffre = i;
             }
 
         }
+        int[] chiffreIntCounter = new int[28];
+        for (int i = 0; i <= laengeChiffre; i++) {
+            if(charToInt(chiffre[i]) == -1)
+            {
+                //System.out.println("Der Fehler sitzt an der Stelle: "+i);
+                i = 100000000;
+
+            }else {
 
 
+                chiffreIntCounter[charToInt(chiffre[i])]++;
+            }
+
+        }
+        //die Höchsten Zwei Zahlen finden
+        int[] hochesteZahlen = new int[2];
+        int hochsteZahl1 = 0;
+        int hochsteZahl2 = 0;
+        for (int i = 0; i < chiffreIntCounter.length; i++) {
+            if(chiffreIntCounter[i] > hochesteZahlen[0])
+            {
+                hochesteZahlen[1] = hochesteZahlen[0];
+                hochsteZahl2 = hochsteZahl1;
+                hochsteZahl1 = i;
+                hochesteZahlen[0] = chiffreIntCounter[i];
+            } else if (chiffreIntCounter[i] > hochesteZahlen[1]) {
+                hochesteZahlen[1] = chiffreIntCounter[i];
+                hochsteZahl2 = i;
+
+            }
+
+        }
+        //System.out.println("Nächste Reihe: ");
+        //System.out.println("Der Bustabe: "+intToChar(hochsteZahl1)+ " wurde am häufigsten getippt!");
+        //System.out.println("Der Bustabe: "+ intToChar(hochsteZahl2)+" wurde am zweithäufgisten verwendet!");
+        // ab hier gilt es nur noch für die Schlüsselwortlänge eins.
+        //System.out.println("Ein Möglicher Versuch lautet: ");
+        for (int i = 0; i < chiffre.length; i++) {
+            char c = intToChar(enc(charToInt(chiffre[i]), berechneVerschiebung(hochsteZahl1)));
+            //System.out.print(c);
+            ErgebnisChar[ErgebnisCharZaehler] = c;
+            ErgebnisCharZaehler++;
+
+        }
+        //System.out.println();
+        //System.out.println("Fertig!");
+
+
+        return ErgebnisChar;
     }
 
+    public static int berechneVerschiebung(int haufigsterBustabe_Parameter)
+    {
+        return HaufgisterBustabeInDeutsch-haufigsterBustabe_Parameter;
+    }
+
+    public static int enc(int chiffre, int verschiebung)
+    {
+        int ergebnis = chiffre + verschiebung;
+        if (ergebnis >26)
+        {
+            ergebnis = ergebnis - 26;
+        }else if (ergebnis< 0)
+        {
+            ergebnis = ergebnis + 26;
+        }
+
+        return ergebnis;
+    }
     public static int charToInt(char c)
     {
 
         switch (c){
             case 'a':
                 return 0;
-                break;
             case 'b':
                 return 1;
-                break;
             case 'c':
                 return 2;
-                break;
             case 'd':
                 return 3;
-                break;
             case 'e':
-                return
-                break;
+                return 4;
             case 'f':
-                return;
-                break;
+                return 5;
             case 'g':
-                return;
-                break;
+                return 6;
             case 'h':
-                return;
-                break;
+                return 7;
             case 'i':
-                return;
-                break;
+                return 8;
             case 'j':
-                return;
-                break;
+                return 9;
             case 'k':
-                return;
-                break;
+                return 10;
             case 'l':
-                return;
-                break;
+                return 11;
             case 'm':
-                return;
-                break;
+                return 12;
             case 'n':
-                return;
-                break;
+                return 13;
             case 'o':
-                return;
-                break;
+                return 14;
             case 'p':
-                return;
-                break;
+                return 15;
             case 'q':
-                return;
-                break;
+                return 16;
             case 'r':
-                return;
-                break;
+                return 17;
             case 's':
-                return;
-                break;
+                return 18;
             case 't':
-                return;
-                break;
+                return 19;
             case 'u':
-                return;
-                break;
+                return 20;
             case 'v':
-                return;
-                break;
+                return 21;
             case 'w':
-                return;
-                break;
+                return 22;
             case 'x':
-                return;
-                break;
+                return 23;
             case 'y':
-                return;
-                break;
+                return 24;
             case 'z':
-                return;
-                break;
+                return 25;
             case '.':
-                return 27;
-                break;
+                return 26;
+            default:
+                //System.out.println("Hier gibt es ein Problem bei dem Zeichen: "+c);
+                return -1;
+        }
+    }
+
+    public static char intToChar(int zahl)
+    {
+
+        switch (zahl){
+            case 0:
+                return 'a';
+            case 1:
+                return 'b';
+            case 2:
+                return 'c';
+            case 3:
+                return 'd';
+            case 4:
+                return 'e';
+            case 5:
+                return 'f';
+            case 6:
+                return 'g';
+            case 7:
+                return 'h';
+            case 8:
+                return 'i';
+            case 9:
+                return 'j';
+            case 10:
+                return 'k';
+            case 11:
+                return 'l';
+            case 12:
+                return 'm';
+            case 13:
+                return 'n';
+            case 14:
+                return 'o';
+            case 15:
+                return 'p';
+            case 16:
+                return 'q';
+            case 17:
+                return 'r';
+            case 18:
+                return 's';
+            case 19:
+                return 't';
+            case 20:
+                return 'u';
+            case 21:
+                return 'v';
+            case 22:
+                return 'w';
+            case 23:
+                return 'x';
+            case 24:
+                return 'y';
+            case 25:
+                return 'z';
+            case 26:
+                return '.';
             default:
                 System.out.println("Hier gibt es ein Problem");
-                break;
+                return 28;
         }
     }
 
---
+++ src/Aufgabe8/Internet.java
@@ -0,0 +1,138 @@
+package Aufgabe8;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//wtlfpqktzbgdxwjswcrfxwjrlcyladajwhraeszqvlqakhawsspfccozepyoqfrh{nfupxxlwbquqhxuctaipbdlaiuqjkdkgighxq{yfbawjplktfgeohyplaxhsijdywuubtzwsfhvoblyeaeszqwtlfpyohjnfmjhbiwwrfpqktzbgdxwjswcrfxwjrlcyeupjtztdgghcvodyqes
+
+public class Internet extends Thread{
+
+
+    public static Internet[] threadStack = new Internet[28];
+
+
+
+
+        public static char[] crypt(char[] plain, char[] key, int direction) {
+
+            char[] output = new char[plain.length];
+            for (int i = 0; i < plain.length; i++) {
+                //Verschluesseln
+                if (direction == 1) {
+                    //Umrechnen für ASCII Tabelle
+                    int result = (plain[i] + key[i % key.length]-194) % 27 ;
+                    output[i] = (char) (result+97);
+                }
+                //Entschluesseln
+                else if (direction == 0){
+                    int result;
+                    if (plain[i] - key[i % key.length] < 0)
+                    {
+                        result = (plain[i]- key[i % key.length]+130) %27 + 97;
+                    }
+                    else
+                    {
+                        result = (plain[i] - key[i % key.length]) % 27 +97;
+                    }
+                    output[i] = (char) result;
+                }
+            }
+            return output;
+        }
+    private int id;
+        public Internet(int id_parameter)
+        {
+            id =id_parameter;
+        }
+        public static void main(String[] args) {
+
+            /*Scanner scanner = new Scanner(System.in);
+            System.out.println("Klartext eingeben:");*/
+
+
+            for (int i = 0; i < 28; i++) {
+                threadStack[i] = new Internet(i);
+                threadStack[i].start();
+
+            }
+            /*char[] decrypted = crypt(plain, key, 0);
+            System.out.println("Entschluesselter Text:");
+            System.out.println(decrypted);
+            scanner.close();*/
+            System.out.println("Fertig");
+        }
+
+    @Override
+    public void run() {
+        String plaintext = "wtlfpqktzbgdxwjswcrfxwjrlcyladajwhraeszqvlqakhawsspfccozepyoqfrh{nfupxxlwbquqhxuctaipbdlaiuqjkdkgighxq{yfbawjplktfgeohyplaxhsijdywuubtzwsfhvoblyeaeszqwtlfpyohjnfmjhbiwwrfpqktzbgdxwjswcrfxwjrlcyeupjtztdgghcvodyqes";//scanner.nextLine(); //ohne Umlaute
+        plaintext = plaintext.toLowerCase();
+        char[] plain = plaintext.toCharArray();
+
+            /*System.out.println("Schluesselwort eingeben:");
+            String keyword = scanner.nextLine();
+            keyword = keyword.toLowerCase();
+            char[] key = keyword.toCharArray();*/
+
+            /*char[] encrypted = crypt(plain, key, 1);
+            System.out.println("Verschluesselter Text:");
+            System.out.println(encrypted);*/
+        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{'};
+        String krypto = "krypto";
+        String key = "";
+        System.out.println("Thread nr. "+id+" ist gestartet");
+        for (int i = id; i < 28; i++) {
+            if(i>0) {
+                key = key+alphabet[i - 1];
+            }
+            for (int j = 0; j < 28; j++) {
+                System.out.println("Thread nr. " + id+" ist bei Nr.: "+j);
+                if(j>0) {
+                    key = key+alphabet[j- 1];
+                }
+                for (int k = 0; k < 28; k++) {
+                    if(k>0) {
+                        key = key+alphabet[k - 1];
+                    }
+                    for (int l = 0; l < 28; l++) {
+                        if(l>0) {
+                            key = key+alphabet[l - 1];
+                        }
+                        for (int m = 0; m < 28; m++) {
+                            if(m>0) {
+                                key = key+alphabet[m - 1];
+                            }
+                            for (int n = 0; n < 28; n++) {
+                                if(n>0) {
+                                    key = key+alphabet[n - 1];
+                                }
+                                for (int o = 0; o < 28; o++) {
+                                    if(o>0) {
+                                        key = key+alphabet[o - 1];
+                                    }
+                                    if(key == "")
+                                    {
+                                        key = "a";
+                                    }
+                                    char[] keyChar = key.toCharArray();
+                                    char[] decrypted = crypt(plain, keyChar, 0);
+                                    for (int p = 0; p < decrypted.length-4; p++) {
+                                        if (decrypted[p] == 'k' && decrypted[p+1] == 'r' && decrypted[p+2] == 'y' && decrypted[p+3] == 'p' && decrypted[p+4] == 't') {
+                                            System.out.println("Entschluesselter Text:");
+                                            System.out.println(decrypted);
+                                        }
+                                    }
+                                    key = "";
+
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        System.out.println("Thread Nr.: "+id+" ist fertig!!!");
+    }
+}
+
